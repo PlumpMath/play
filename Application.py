2374e4ef3ed213f084dfe971b83d2d3dc521158c
Maria McKinley <mariak@mariakathryn.net>
2015-01-03 01:12:21 -0800

    added a application modeled on panda3d manual tutorial

---
+++ Application.py
@@ -0,0 +1,35 @@
+from direct.showbase.ShowBase import ShowBase
+from direct.showbase.RandomNumGen import RandomNumGen
+from direct.actor.Actor import Actor
+from panda3d.core import Vec3
+from direct.interval.IntervalGlobal import *
+
+#from pandac.PandaModules import loadPrcFileData
+
+#loadPrcFileData("", "want-directtools #t")
+#loadPrcFileData("", "want-tk #t")
+
+
+class Application(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.panda = Actor("panda", {"walk": "panda-walk"})
+        self.panda.reparentTo(render)
+        self.panda.setHpr(-90, 0, 0)
+        self.panda.loop("walk")
+        self.walkIval1 = self.panda.posInterval(2, Vec3(-8, 0, 0), startPos=Vec3(8, 0, 0))
+        self.walkIval2 = self.panda.posInterval(2, Vec3(8, 0, 0), startPos=Vec3(-8, 0, 0))
+        self.turnIval1 = self.panda.hprInterval(0.5, Vec3(90, 0, 0), startHpr=Vec3(-90, 0, 0))
+        self.turnIval2 = self.panda.hprInterval(0.5, Vec3(-90, 0, 0), startHpr=Vec3(90, 0, 0))
+        self.colorIval = Func(self.randomColor)
+        #self.pandaWalk = Sequence(self.walkIval1, self.turnIval1, self.colorIval, self.walkIval2, self.turnIval2, self.colorIval)
+        #self.pandaWalk.loop()
+        self.pandaAnim = ActorInterval(self.panda, "walk", loop=1, duration=5)
+        self.pandaMove = Sequence(self.walkIval1, self.turnIval1, self.colorIval, self.walkIval2, self.turnIval2, self.colorIval)
+        self.pandaWalk = Parallel(self.pandaAnim, self.pandaMove)
+        self.pandaWalk.loop()
+        self.cam.setPos(0, -50, 6)
+
+    def randomColor(self):
+        rand = RandomNumGen(globalClock.getFrameTime())
+        self.panda.setColorScale(rand.random(), rand.random(), rand.random(), 255)
