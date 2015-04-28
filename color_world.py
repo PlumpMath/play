dd35255d11cee1631eaa14b6b658931e215b4a78
Maria <parody@uw.edu>
2015-04-28 15:48:35 -0700

    now plotting in a second window. joystick and keyboard working together as desired

--- color_world.py
+++ color_world.py
@@ -1,7 +1,9 @@
 from direct.showbase.ShowBase import ShowBase
 from direct.showbase.DirectObject import DirectObject
-from direct.actor.Actor import ActorNode
+from direct.actor.Actor import ActorNode, Camera
 from panda3d.core import WindowProperties, NodePath, LVector3, KeyboardButton
+from panda3d.core import GeomNode, LineSegs
+from color import make_square
 import sys
 try:
     import pygame
@@ -19,10 +21,9 @@ class ColorWorld(DirectObject):
         # joystick
         js_count = 0
         self.joystick = None
+        map_avatar = True
         if pygame:
-            # try getting rid of one of the joystick inits
             pygame.init()
-            pygame.joystick.init()
             js_count = pygame.joystick.get_count()
         if js_count > 1:
             raise NotImplementedError("More than one Joystick is connected")
@@ -32,19 +33,28 @@ class ColorWorld(DirectObject):
             self.joystick.init()
             print 'joystick open'
             print self.joystick.get_numaxes()
-            # with keyboard polling, I get a response as long as the key is pressed,
-            # so I don't have to remember the state, but with the joystick, I only
-            # get a signal if things change, so have to remember previous velocity
+            # keep track of velocity, this allows me to counteract joystick with keyboard
             self.velocity = LVector3(0)
             # threshold for joystick
             self.threshold = 0.1
         print self.joystick
+        # map avatar variables
+        self.render2 = None
+        self.render2d = None
+        self.last_avt = [0, 0]
+        self.map_avt_node = []
+        # need a multiplier to the joystick output to tolerable speed
+        self.vel_base = 3
+        self.max_vel = [500, 500, 0]
+
         self.base = ShowBase()
-        # Hide mouse
         props = WindowProperties()
         props.setCursorHidden(True)
         self.base.win.requestProperties(props)
         self.base.disableMouse()
+
+        if map_avatar:
+            self.setup_display2()
 
         courtyard = self.base.loader.loadModel('../goBananas/models/play_space/round_courtyard.bam')
         courtyard.reparentTo(self.base.render)
@@ -58,8 +68,6 @@ class ColorWorld(DirectObject):
         self.base.camera.setPos(0, 0, 0)
         self.avatar.setPos(-10, -10, 2)
 
-        self.vel_base = 3
-        self.max_vel = [500, 500, 0]
         self.setup_inputs()
         self.frameTask = self.base.taskMgr.add(self.frame_loop, "frame_loop")
         self.frameTask.last = 0  # task time of the last frame
@@ -70,13 +78,20 @@ class ColorWorld(DirectObject):
         task.last = task.time
         self.poll_joystick()
         self.poll_keyboard()
-        self.move_avatar(dt)
+        move = self.move_avatar(dt)
+        self.move_map_avatar(move)
         return task.cont
 
     def poll_joystick(self):
         if not self.joystick:
             return
         # joystick input -1 to 1,
+        # if I get the event, it only has a signal when
+        # there is movement, so loop through the events,
+        # to collect events into axis call, but call
+        # axis after, since it will stay at whatever the
+        # last signal was, instead of zeroing out whenever
+        # no movement. Much more convenient that way.
         for ev in pygame.event.get():
             pass
         x = self.joystick.get_axis(0)
@@ -94,8 +109,10 @@ class ColorWorld(DirectObject):
             self.velocity.y = -y
 
     def poll_keyboard(self):
+        # under normal circumstances, use median of joytick output
         x_speed = 0.5
         y_speed = 0.5
+        # checks keyboard, not mouse, in this case
         is_down = self.base.mouseWatcherNode.is_button_down
         # Instead of usual movement, exactly counteract joystick,
         # if using joystick and currently moving
@@ -125,6 +142,7 @@ class ColorWorld(DirectObject):
         # this makes for smooth (correct speed) diagonal movement
         # print 'velocity', self.velocity
         magnitude = max(abs(self.velocity[0]), abs(self.velocity[1]))
+        move = None
         if self.velocity.normalize():
             # go left in increasing amount
             # print 'dt', dt
@@ -134,10 +152,55 @@ class ColorWorld(DirectObject):
             self.velocity *= magnitude
             # print 'velocity', self.velocity
             # this makes for smooth movement
-            self.avatar.setFluidPos(self.avatar, self.velocity * self.vel_base * dt)
+            move = self.velocity * self.vel_base * dt
+            self.avatar.setFluidPos(self.avatar, move)
+        return move
+
+    def move_map_avatar(self, move):
+        # print move
+        if move:
+            move *= 0.01
+            avt = LineSegs()
+            avt.setThickness(1)
+            avt.setColor(1, 1, 1)
+            # print 'last', self.last_avt
+            avt.move_to(self.last_avt[0], 1, self.last_avt[1])
+            new_move = [i + j for i, j in zip(self.last_avt, move)]
+            # print 'new', new_move
+            self.last_avt = [new_move[0], new_move[1]]
+            avt.draw_to(new_move[0], 1, new_move[1])
+            self.map_avt_node.append(self.render2d.attach_new_node(avt.create()))
+            # can't let too many nodes pile up
+            if len(self.map_avt_node) > 299:
+                for i, j in enumerate(self.map_avt_node):
+                    j.removeNode()
+                    if i > 49:
+                        break
+                del self.map_avt_node[0:50]
 
     def setup_inputs(self):
         self.accept('q', self.close)
+
+    def setup_display2(self):
+        props = WindowProperties()
+        props.setCursorHidden(True)
+        props.setSize(400, 400)
+        props.setOrigin(128, 128)
+        window2 = self.base.openWindow(props=props, aspectRatio=1)
+        self.render2 = NodePath('render2')
+        camera = self.base.camList[-1]
+        camera.reparentTo(self.render2)
+        camera.setPos(0, -5, 0)
+        square = make_square(-1, -1, -1, 1, -1, 1)
+        sq_node = GeomNode('square')
+        sq_node.addGeom(square)
+        self.render2.attach_new_node(sq_node)
+        print 'render2', self.render2
+        self.render2d = NodePath('render2d')
+        camera2d = self.base.makeCamera(window2)
+        camera2d.reparentTo(self.render2d)
+        # env = self.base.loader.loadModel('environment.egg')
+        # env.reparentTo(self.render2)
 
     def close(self):
         pygame.quit()
